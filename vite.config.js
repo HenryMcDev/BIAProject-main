3645484a11c23f10b363effcd21c4f7deb942b35
HenryMcDev <henryoficial37@gmail.com>
2026-04-23 17:20:41 -0300

    chore: simplify Dockerfile to run Vite preview server and update configuration base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react(), tailwindcss()],
   server: {
     // Tente usar true em vez de 'all'
